c9a55fe34c5749ae5770e6e20ffc528b380cabfa
Vipin Kumawat <kumawatvipin066@gmail.com>
2026-08-06 18:12:02 +0530

    webhooks

--- src/graxon/webhooks/types.py
+++ src/graxon/webhooks/types.py
@@ -1,5 +1,52 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import datetime
+import uuid
 
 
 class WebhookCreateParams(BaseModel):
-    pass
+    org_id: str = Field(
+        description="The organization id of the Webhook",
+    )
+    project_id: uuid.UUID = Field(
+        description="The project id of the Webhook",
+    )
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        description="The id of the document",
+    )
+    name: str = Field(
+        description="The name of the Webhook",
+    )
+    url: str = Field(
+        description="The url of the Webhook",
+    )
+    token: str = Field(
+        description="The token of the Webhook, will be pass in header 'X-GRAXON-TOKEN' to the request",
+    )
+
+
+class WebhookResponseParams(BaseModel):
+    id: uuid.UUID = Field(
+        description="The id of the Webhook",
+    )
+    org_id: str = Field(
+        description="The organization id of the Webhook",
+    )
+    project_id: uuid.UUID = Field(
+        description="The project id of the Webhook",
+    )
+    name: str = Field(
+        description="The name of the Webhook",
+    )
+    url: str = Field(
+        description="The url of the Webhook",
+    )
+    token: str = Field(
+        description="The token of the Webhook, will be pass in header 'X-GRAXON-TOKEN' to the request",
+    )
+    created_at: datetime.datetime = Field(
+            description="The created at of the organization",
+    )
+    updated_at: datetime.datetime = Field(
+        description="The updated at of the organization",
+    )
