aa09e28fab296e84955265a993e4fb2f06b220e9
Vipin Kumawat <kumawatvipin066@gmail.com>
2026-08-06 18:12:13 +0530

    projetcs

--- src/graxon/projects/types.py
+++ src/graxon/projects/types.py
@@ -1,5 +1,46 @@
-from pydantic import BaseModel
+from ..project_configs.types import ProjectConfigCreateParams
+from typing import Optional, Dict, Any
+from pydantic import BaseModel, Field
+import datetime
+import uuid
 
 
 class ProjectCreateParams(BaseModel):
-    pass
+    org_id: str = Field(
+        description="The organization id of the project",
+    )
+    name: str = Field(
+        description="The name of the project",
+    )
+    config: ProjectConfigCreateParams
+    description: str = Field(
+        description="The description of the project",
+    )
+    project_metadata: Optional[Dict[str, Any]] = Field(
+        default={},
+        description="The project metadata of the project",
+    )
+
+
+class ProjectResponseParams(BaseModel):
+    id: uuid.UUID = Field(
+        description="The id of the project",
+    )
+    readable_id: str = Field(
+        description="The readable id of the project",
+    )
+    org_id: str = Field(
+        description="The organization id of the project",
+    )
+    name: str = Field(
+        description="The name of the project",
+    )
+    description: str = Field(
+        description="The description of the project",
+    )
+    created_at: datetime.datetime = Field(
+        description="The created at of the project",
+    )
+    updated_at: datetime.datetime = Field(
+        description="The updated at of the project",
+    )
